0d1b0c7f5fefa48a1eb207620fc004baf4de4c5d
sujj <mm4324@gmail.com>
2017-03-04 01:30:54 +0800

    delete useless file

--- chrome-extension/contents.js
+++
@@ -1,9 +0,0 @@
-
-var page = document.body.innerText;
-
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-	if (msg.text === 'get_inner_text') {
-    	sendResponse(page);
-    	return true;
-	}
-});
